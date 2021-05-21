e89d135d2c4689a809b3be9160f53aedb0d9a64b
SnowFlame <genylep@mail.ru>
2021-05-21 12:11:52 +0300

    реализовал smooth scroll и табы

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,14 @@
-const featureLinksAll = document.querySelectorAll('.feature__link');
-const featureSubsAll = document.querySelectorAll('.feature-sub')
-
-featureLinksAll.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    featureLinksAll.forEach((featureLink, i) => {
-      i === index ? featureLink.classList.toggle('feature__link_active') : featureLink.classList.remove('feature__link_active')
-    })
-
-    featureSubsAll.forEach((featureSub, i) => {
-      i === index ? featureSub.classList.toggle('hidden') : featureSub.classList.add('hidden')
-    })
-  })
-})  
+const featureLinkElems = document.querySelectorAll('.feature__link');
+const featureSubElems = document.querySelectorAll('.feature-sub')
 
 featureLinkElems.forEach((btn, index) => {
   btn.addEventListener('click', () => {
-      featureSubElems.forEach((featureSubElem, i) => {
-          console.log(i, index);
-          i === index ? featureSubElem.classList.toggle('hidden') : featureSubElem.classList.add('hidden');
-      })
-      featureLinkElems.forEach((featureLinkElem, i) => {
-          i === index ? featureLinkElem.classList.toggle('feature__link_active') : featureLinkElem.classList.remove('feature__link_active');
-      })   
+    featureLinkElems.forEach((featureLink, i) => {
+      i === index ? featureLink.classList.toggle('feature__link_active') : featureLink.classList.remove('feature__link_active')
+    })
+
+    featureSubElems.forEach((featureSub, i) => {
+      i === index ? featureSub.classList.toggle('hidden') : featureSub.classList.add('hidden')
+    })
   })
 })
